a13af302c4560528fdf8ac1adeed3ca21358de0d
Sakib Biswas <sakibsakib99880@gmail.com>
2025-05-05 14:58:23 +0600

    class-obj

---
+++ module3/src/3.1.ts
@@ -0,0 +1,26 @@
+{
+    // oop -class
+
+class Animal{
+    name:string;
+    specises:string;
+    sound:string;
+    constructor(name:string,specises:string,sound:string){
+        this.name=name;
+        this.sound=sound;
+        this.specises=specises;
+    }
+
+    makesound(){
+        console.log(`The ${this.name} says ${this.sound}`)
+    }
+}
+const dog=new Animal('German Shepard Bhai','dog','gew gew')
+  const cat=new Animal('Persian','cat','mew mew')
+  cat.makesound()
+dog.makesound() 
+
+
+
+
+}
